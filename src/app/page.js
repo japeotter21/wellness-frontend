c2445208fd47ddac3d784f582e783e62187314ff
john potter <juanpotter7@gmail.com>
2024-01-24 22:52:00 -0500

    init

--- src/app/page.js
+++ src/app/page.js
@@ -1,113 +1,191 @@
+"use client"
+import React, { useState, useEffect } from 'react'
 import Image from 'next/image'
+import axios from 'axios'
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    const [caffeine, setCaffeine] = useState(0)
+    const [water, setWater] = useState('')
+    const [sleep, setSleep] = useState(0)
+    const [wakeUp, setWakeUp] = useState('')
+    const [quality, setQuality] = useState(0)
+    const [showData, setShowData] = useState(false)
+    const [allData, setAllData] = useState({})
+    const cModal = typeof document !== 'undefined' && document.getElementById("caffeinemodal")
+    const sModal = typeof document !== 'undefined' && document.getElementById("sleepmodal")
+    const wModal = typeof document !== 'undefined' && document.getElementById("watermodal")
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+    function PostCaf() {
+        const postObj = {
+            mg: parseInt(caffeine)
+        }
+        axios.post('http://127.0.0.1:8000/caffeine/', postObj)
+        .then(res => {
+            console.log(res.data)
+            setCaffeine(0)
+        })
+    }
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+    function PostWater() {
+        const postObj = {
+            oz: parseInt(water)
+        }
+        axios.post('http://127.0.0.1:8000/water/', postObj)
+        .then(res => {
+            console.log(res.data)
+            setWater(0)
+        })
+    }
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+    function PostSleep() {
+        const postObj = {
+            hours: parseInt(sleep),
+            quality: parseInt(quality),
+            awake: wakeUp
+        }
+        axios.post('http://127.0.0.1:8000/sleep/', postObj)
+        .then(res => {
+            console.log(res.data)
+            setSleep(0)
+            setQuality(0)
+            setWakeUp('')
+        })
+    }
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
+    function GetData() {
+        axios.all(['http://127.0.0.1:8000/sleep','http://127.0.0.1:8000/caffeine','http://127.0.0.1:8000/caffeine'].map(url=>axios.get(url)))
+        .then(axios.spread((slp, caf, wat)=>{
+            const slpField = slp.data
+            const cafField = caf.data
+            const watField = wat.data
+            setAllData({
+                sleep: slpField,
+                caffeine: cafField,
+                water: watField
+            })
+            setShowData(true)
+        }))
+    }
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+    function CloseModal(obj) {
+        obj.close()
+        setCaffeine(0)
+    }
+    console.log(wakeUp)
+
+    return (
+        <main className="flex min-h-screen flex-col items-center justify-between xl:p-24 pt-24 bg-gradient-radial from-slate-700 to-slate-500 text-white font-sans">
+            <div className='grid grid-cols-2 xl:w-1/2 w-11/12'>
+                <button className='flex flex-col items-center bg-gradient-radial from-orange-500 to-amber-400 rounded-tl-md px-6 py-12'
+                    onClick={()=>sModal.showModal()}
+                >
+                    <p>Zzz.png</p>
+                    <p className='font-light text-sm'>Sleep</p>
+                </button>
+                <button className='flex flex-col items-center bg-gradient-radial from-blue-500 to-blue-400 rounded-tr-md px-6 py-12'
+                    onClick={()=>cModal.showModal()}
+                >
+                    <p>Coffee.png</p>
+                    <p className='font-light text-sm'>Caffeine</p>
+                </button>
+                <button className='flex flex-col items-center bg-gradient-radial from-purple-500 to-purple-400 px-6 py-12'>
+                    <p>Weights.png</p>
+                    <p className='font-light text-sm'>Exercise</p>
+                </button>
+                <button className='flex flex-col items-center bg-gradient-radial from-green-500 to-emerald-400 px-6 py-12'>
+                    <p>Face.png</p>
+                    <p className='font-light text-sm'>Mood</p>
+                </button>
+                <button className='flex flex-col items-center bg-gradient-radial from-sky-500 to-sky-400 px-6 py-12'
+                    onClick={()=>wModal.showModal()}
+                >
+                    <p>glass.png</p>
+                    <p className='font-light text-sm'>Water</p>
+                </button>
+                <button className='flex flex-col items-center bg-gradient-radial from-pink-500 to-pink-400 px-6 py-12'
+                >
+                    <p>Glasses.png</p>
+                    <p className='font-light text-sm'>Focus</p>
+                </button>
+                <button className='flex flex-col items-center bg-gradient-radial from-red-500 to-red-400 rounded-bl-md px-6 py-12'>
+                    <p>Bottle.png</p>
+                    <p className='font-light text-sm'>Alcohol</p>
+                </button>
+                <button className='flex flex-col items-center bg-gradient-radial from-gray-500 to-gray-400 rounded-br-md px-6 py-12'
+                    onClick={GetData}
+                >
+                    <p>Chart.png</p>
+                    <p className='font-light text-sm'>All Stats</p>
+                </button>
+            </div>
+            { showData ? 
+                <div className='w-3/4 break-words py-8'>
+                    {JSON.stringify(allData)}                    
+                </div>
+            :
+                <></>
+            }
+            <dialog id="caffeinemodal">
+                <div className='p-4 flex flex-col gap-4'>
+                    <h3 className='text-lg'>Caffeine Consumed:</h3>
+                    <div>
+                        <p className='text-sm text-neutral-500'>mg:</p>
+                        <input type="number" className='border px-3 py-2 rounded-md' value={caffeine}
+                            onChange={(e)=>setCaffeine(e.target.value)}
+                        />
+                    </div>
+                    <div className='flex justify-between items-center mt-4'>
+                        <button onClick={()=>CloseModal(cModal)}>Close</button>
+                        <button onClick={PostCaf}>Submit</button>
+                    </div>
+                </div>
+            </dialog>
+            <dialog id="watermodal">
+                <div className='p-4 flex flex-col gap-4'>
+                    <h3 className='text-lg'>Water Consumed:</h3>
+                    <div>
+                        <p className='text-sm text-neutral-500'>oz:</p>
+                        <input type="number" className='border px-3 py-2 rounded-md' value={water}
+                            onChange={(e)=>setWater(e.target.value)}
+                        />
+                    </div>
+                    <div className='flex justify-between items-center mt-4'>
+                        <button onClick={()=>CloseModal(wModal)}>Close</button>
+                        <button onClick={PostWater}>Submit</button>
+                    </div>
+                </div>
+            </dialog>
+            <dialog id="sleepmodal">
+                <div className='p-4 flex flex-col gap-4'>
+                    <h3 className='text-lg'>Sleep:</h3>
+                    <div>
+                        <p className='text-sm text-neutral-500'>Hours</p>
+                        <input type="number" className='border px-3 py-2 rounded-md' value={sleep}
+                            onChange={(e)=>setSleep(e.target.value)}
+                        />
+                    </div>
+                    <div>
+                        <p className='text-sm text-neutral-500'>Woke up at:</p>
+                    <input type="time" className='border px-3 py-2 rounded-md w-full' value={wakeUp}
+                        onChange={(e)=>setWakeUp(e.target.value)}
+                    />
+                    </div>
+                    <div>
+                        <p className='text-sm text-neutral-500'>Quality (1: worst - 5: best)</p>
+                        <select type="number" className='border px-3 py-2 rounded-md w-full' value={quality}
+                            onChange={(e)=>setQuality(e.target.value)}
+                        >
+                            {[1,2,3,4,5].map((item,id)=>
+                                <option value={item} key={id}>{item}</option>
+                            )}
+                        </select>
+                    </div>
+                    <div className='flex justify-between items-center mt-4'>
+                        <button onClick={()=>CloseModal(sModal)}>Close</button>
+                        <button onClick={PostSleep}>Submit</button>
+                    </div>
+                </div>
+            </dialog>
+        </main>
+    )
 }
